acfefb2562a860c4d6b5aaca1ec6dc7538cc6028
Michael (Mike) Williams <34947926+mpwusr@users.noreply.github.com>
2026-01-31 12:20:12 -0500

    commit new LICENSE MIT providers.tf and spark-pi example

---
+++ terraform/providers.tf
@@ -0,0 +1,23 @@
+variable "kubeconfig_path" {
+  type        = string
+  description = "Path to kubeconfig file used by kubectl (merged config)."
+  default     = "~/.kube/config"
+}
+
+variable "kube_context" {
+  type        = string
+  description = "Kubeconfig context name for Rancher Desktop cluster."
+  default     = "rd"
+}
+
+provider "kubernetes" {
+  config_path    = pathexpand(var.kubeconfig_path)
+  config_context = var.kube_context
+}
+
+provider "helm" {
+  kubernetes {
+    config_path    = pathexpand(var.kubeconfig_path)
+    config_context = var.kube_context
+  }
+}
